cf478235849e0877182e9406541bc20dd8cc4319
Jakob Troidl <jakob.troidl@googlemail.com>
2023-10-02 15:02:40 -0400

    fix

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import { createMLP, from_tfjs } from "web-mlp";
 async function testMLP() {
   let batch_size = 70000;
   let tile_size = 8; // must not be bigger than 16
-  const path = "https://jakobtroidl.github.io/data/trainedModel/model.json"; // path to tensorflow.js model
+
+  const path =
+    "https://jakobtroidl.github.io/data/trainedModelOriginal/model.json"; // path to tensorflow.js model
 
   let tfjs_model = await from_tfjs(path); // load tfjs model
   let model = await createMLP(tfjs_model, batch_size, tile_size); // convert model for fast inference
