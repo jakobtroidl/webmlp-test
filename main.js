e0cc01430bf017a65cdea934148662ec647518fb
Jakob Troidl <jakob.troidl@googlemail.com>
2023-10-02 14:13:49 -0400

    add src

---
+++ main.js
@@ -0,0 +1,18 @@
+import { createMLP, from_tfjs } from "web-mlp";
+
+async function testMLP() {
+  let batch_size = 70000;
+  let tile_size = 8; // must not be bigger than 16
+  const path = "https://jakobtroidl.github.io/data/trainedModel/model.json"; // path to tensorflow.js model
+
+  let tfjs_model = await from_tfjs(path); // load tfjs model
+  let model = await createMLP(tfjs_model, batch_size, tile_size); // convert model for fast inference
+  let X = Float32Array.from(Array(batch_size * model.inputSize).fill(0), () =>
+    Math.random()
+  ); // generate random a input
+
+  let result = await model.inference(X); // inference the model
+  console.log("result", result); // plot predictions
+}
+
+testMLP();
